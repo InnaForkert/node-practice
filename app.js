5102782c015b7bff855ad52647d3080a36ae537c
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-22 22:22:22 +0200

    input to file

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
 function rqListener(req, res) {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Hi</title></head>");
@@ -10,6 +12,22 @@ function rqListener(req, res) {
     );
     res.write("</head>");
     return res.end();
+    0;
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    res.on("data", (chunck) => {
+      console.log(chunck);
+      body.push(chunck);
+    });
+    res.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end;
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
