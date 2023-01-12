d1314101a6e74357330e431a4e19736141d7c6fb
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-12 23:59:20 +0200

    les 67

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("alsways");
-  next();
-});
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/add-product", (req, res, next) => {
-  res.send("<h1>Add!</h1>");
-});
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello!</h1>");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
