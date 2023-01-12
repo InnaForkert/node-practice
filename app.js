811e558a96376f2c7ceba30d2910fadf5fa07d10
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-12 23:36:04 +0200

    lection 3

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const http = require("http");
+const express = require("express");
 
-const routes = require("./routes");
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use("/", (req, res, next) => {
+  console.log("alsways");
+  next();
+});
 
-server.listen(3000);
+app.use("/add-product", (req, res, next) => {
+  res.send("<h1>Add!</h1>");
+});
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello!</h1>");
+});
+
+app.listen(3000);
