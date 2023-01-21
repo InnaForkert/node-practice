b13d0083830091d1adccc3aced9ca3b6df43bef4
Inna <inna.forkert@gmail.com>
2023-01-21 15:25:49 +0200

    module 6

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const shopRoutes = require("./routes/shop");
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
+// const expressHbs = require("express-handlebars");
 
 const app = express();
+
+// app.engine("hbs", expressHbs());
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
