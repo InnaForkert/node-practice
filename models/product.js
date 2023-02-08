891554ab92086902cc5ee8cb77fb9cab783ef96f
Inna <inna.forkert@gmail.com>
2023-02-08 03:59:24 +0200

    feat: dynamic routes

--- models/product.js
+++ models/product.js
@@ -2,26 +2,31 @@ const fs = require("fs");
 const path = require("path");
 
 const p = path.join(
-  path.dirname(require.main.filename),
+  path.dirname(process.mainModule.filename),
   "data",
   "products.json"
 );
 
-const getProductsFromFile = (callback) => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return callback([]);
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    callback(JSON.parse(fileContent));
   });
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -30,7 +35,14 @@ module.exports = class Product {
     });
   }
 
-  static fetchAll(callback) {
-    getProductsFromFile(callback);
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((el) => el.id === id);
+      cb(product);
+    });
   }
 };
